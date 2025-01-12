64ddcf54c1d383c4cbcd5358324bd601488666ef
mjkeller <michaelkeller1997@gmail.com>
2025-01-12 14:50:04 -0500

    finished binary search tree

--- binarySearchTree.c
+++ binarySearchTree.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct treeNode{
     int value;
@@ -46,32 +47,56 @@ void printTree_rec(treeNode *root, int level){
     }
 }
 
+bool insertNumber(treeNode **rootptr, int value){
+    treeNode *root = *rootptr;
+
+    /*Protects from issue when adding first node*/
+    if(root == NULL){
+        /*tree empty*/
+        (*rootptr) = createNode(value);
+        return true;
+    }
+
+    /*not allowing to of the same value in this list*/
+    if(value == root->value){
+        printf("Value exists in tree\n");
+        return false;
+    }
+    else if(value < root->value){
+        return insertNumber(&(root->left),value);
+    }
+    else if (value > root->value){
+        return insertNumber(&(root->right),value);
+    }
+    return false;
+}
+
+bool findNumber(treeNode *root, int value){
+    if(root == NULL) return false;
+    if(root->value == value){
+        return true;
+    }
+    else if (value < root->value){
+        findNumber((root->left), value);
+    }
+    else if (value > root->right){
+        findNumber((root->right),value);
+    }
+}
+
 int main(){
-    treeNode *n1 = createNode(43);
-    treeNode *n2 = createNode(93);
-    treeNode *n3 = createNode(72);
-    treeNode *n4 = createNode(39);
-    treeNode *n5 = createNode(93);
-    treeNode *n6 = createNode(57);
-    treeNode *n7 = createNode(64);
-    treeNode *n8 = createNode(27);
+    treeNode *root = NULL;
 
-    n1->left = n2;
-    n1->right = n3;
-    n2->left = n4;
-    n2->right = n5; 
-    n3->left = n6;
-    n3->right = n7;
-    n4->left = n8;
+    insertNumber(&root,35);
+    insertNumber(&root,20);
+    insertNumber(&root,70);
+    insertNumber(&root,27);
+    insertNumber(&root,45);
+    insertNumber(&root,33);
 
-    printTree_rec(n1, 0);
+    printTree_rec(root, 0);
+
+    printf("Number 20 = %d\n",findNumber(root,20));
+    printf("Number 21 = %d\n",findNumber(root,21));
     
-    free(n1);
-    free(n2);
-    free(n3);
-    free(n4);
-    free(n5);
-    free(n6);
-    free(n7);
-    free(n8);
 }
